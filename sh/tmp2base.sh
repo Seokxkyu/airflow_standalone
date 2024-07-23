4bd929f3fd58937b09ee1bc57a452938fe1aa2a1
Seokxkyu <kyuseok000@gmail.com>
2024-07-23 14:20:41 +0900

    tmp2base.sh modify

--- sh/tmp2base.sh
+++ sh/tmp2base.sh
@@ -10,15 +10,18 @@ DELETE FROM history_db.cmd_usage WHERE dt='${DT}';
 INSERT INTO history_db.cmd_usage
 SELECT 
 	CASE WHEN dt LIKE '%-%-%'
-	THEN STR_TO_DATE(dt, '%Y-%m-%d')
-	ELSE STR_TO_DATE('1970-01-01', '%Y-%m-%d')
+		THEN STR_TO_DATE(dt, '%Y-%m-%d')
+		ELSE STR_TO_DATE('1970-01-01', '%Y-%m-%d')
 	END AS dt,
 	command,
 	CASE WHEN cnt REGEXP '[0-9]+$' 
-	THEN CAST(cnt AS UNSIGNED)
-	ELSE -1
-	END AS cnt
+		THEN CAST(cnt AS UNSIGNED)
+		ELSE -1
+	END AS cnt, 
+	'${DT}' AS tmp_dt
 FROM history_db.tmp_cmd_usage
 WHERE dt = '${DT}'
 ;
 EOF
+
+echo "DONE"
