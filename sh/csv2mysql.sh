ce9573e170654c228426c59da7ed4e9cc16c9b1c
Seokxkyu <kyuseok000@gmail.com>
2024-07-23 09:59:24 +0900

    07-23 sql init

--- sh/csv2mysql.sh
+++ sh/csv2mysql.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
 CSV_PATH=$1
+DEL_DT=$2
 
 user="root"
-password="qwer123"
-database="history_db"
+# password="qwer123"
+# database="history_db"
 
-mysql -u"$user" -p"$password" "$database" <<EOF
-LOAD DATA INFILE '/var/lib/mysql-files/csv.csv'
--- LOAD DATA INFILE '${CSV_PATH}'
+MYSQL_PWD='qwer123' mysql --local-infile=1 -u"$user" <<EOF
+DELETE FROM history_db.tmp_cmd_usage WHERE dt='${DEL_DT}';
+LOAD DATA LOCAL INFILE '$CSV_PATH'
+
 INTO TABLE history_db.tmp_cmd_usage
-FIELDS TERMINATED BY ',' 
-ESCAPED BY ''
+FIELDS 
+	TERMINATED BY ',' 
+	ENCLOSED BY '^' 
+	ESCAPED BY '\b'
 LINES TERMINATED BY '\n';
 EOF
