4f90865cc4fd06c035928da3e297ed972ad8f0e4
Seokxkyu <kyuseok000@gmail.com>
2024-08-02 08:48:53 +0900

    0802 init

--- dags/movie_summary.py
+++ dags/movie_summary.py
@@ -11,6 +11,7 @@ from airflow.operators.python import (
         BranchPythonOperator,
 )
 
+from pprint import pprint 
 
 with DAG(
     'movie_summary',
@@ -29,7 +30,7 @@ with DAG(
     tags=['movie', 'summary', 'dataframe'],
 ) as dag:
     
-    REQUIREMENTS=["git+https://github.com/Seokxkyu/mov.git@0.3/api"]
+    REQUIREMENTS=["git+https://github.com/Seokxkyu/mov_agg.git@0.5/agg"]
 
     def gen_empty(id):
         task = EmptyOperator(task_id=id)
@@ -40,16 +41,13 @@ with DAG(
         return tuple(tasks) # (t, )
 
     def gen_vpython(**kwarg):
-        id = kwarg['id']
-        func = kwarg['func']
-        op_kwargs = kwarg['op_kwargs']
-        
+
         task = PythonVirtualenvOperator(
-            task_id=id, 
-            python_callable=func,
+            task_id=kwarg['id'], 
+            python_callable=kwarg['func'],
             system_site_packages=False,
             requirements=REQUIREMENTS,
-            op_kwargs=op_kwargs
+            op_kwargs=kwarg['op_kwargs']
         )
         return task
     '''
@@ -71,14 +69,32 @@ with DAG(
         # print("pro data")
 
     def pro_data(**params):
+        from pprint import pprint
         print("*" * 30)
         print(params['task_name'])
+        pprint(params) # 여기는 task_name
         print("*" * 30)
 
-    def pro_data2(task_name, **params):
-        print("@" * 30)
-        print(params['task_name'])
-        print("@" * 30)
+    def pro_merge(task_name, **params):
+        load_dt = params['ds_nodash']
+        from mov_agg.util import merge
+        df = merge(load_dt)
+        print("*" * 30)
+        print(df)
+
+    def pro_data3(task_name):
+        print("@" * 33)
+        print(task_name)
+        #print(params) # 여기는 task_name 없을 것으로 예상
+        print("@" * 33)
+    
+    def pro_data4(task_name, ds_nodash, **kwargs):
+        from pprint import pprint
+        print("@" * 33)
+        print(task_name)
+        print(ds_nodash)
+        pprint(kwargs) # 여기는 task_name 없을 것으로 예상, ds_nodash 도 없 ...
+        print("@" * 33)
 
     def merge_df():
         return
@@ -99,10 +115,9 @@ with DAG(
             }
     )
     
-    
     merge_df = gen_vpython(
             id="merge_df",
-            func=pro_data,
+            func=pro_merge,
             op_kwargs={
                 "task_name": "merge_df!!!"
             }
@@ -110,7 +125,7 @@ with DAG(
 
     de_dup = gen_vpython(
             id="de_dup",
-            func=pro_data,
+            func=pro_data3,
             op_kwargs={
                 "task_name": "de_dup!!!"
             }
@@ -118,11 +133,12 @@ with DAG(
 
     summary_df = gen_vpython(
             id="summary_df",
-            func=pro_data,
+            func=pro_data4,
             op_kwargs={
                 "task_name": "summary_df!!!"
             }
     )
 
 
-    start >> apply_type >> merge_df >> de_dup >> summary_df >> end
+    start >> merge_df 
+    merge_df >> de_dup >> apply_type >> summary_df >> end
