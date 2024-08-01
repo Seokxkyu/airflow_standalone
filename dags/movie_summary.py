3174f4609216994ecb601d5c6e71069917ca69a0
Seokxkyu <kyuseok000@gmail.com>
2024-08-01 12:50:35 +0900

    pro data edit

--- dags/movie_summary.py
+++ dags/movie_summary.py
@@ -19,7 +19,7 @@ with DAG(
         'retries': 1,
         'retry_delay': timedelta(seconds=3),
     },
-
+    
     max_active_runs=1,
     max_active_tasks=3,
     description='movie summary',
@@ -28,9 +28,57 @@ with DAG(
     catchup=True,
     tags=['movie', 'summary', 'dataframe'],
 ) as dag:
+    
+    REQUIREMENTS=["git+https://github.com/Seokxkyu/mov.git@0.3/api"]
+
+    def gen_empty(id):
+        task = EmptyOperator(task_id=id)
+        return task
+   
+    def gen_empty(*ids):
+        tasks = [EmptyOperator(task_id=id) for id in ids]
+        return tuple(tasks) # (t, )
+
+    def gen_vpython(**kwarg):
+        id = kwarg['id']
+        func = kwarg['func']
+        op_kwargs = kwarg['op_kwargs']
         
-    def app_type():
-        return
+        task = PythonVirtualenvOperator(
+            task_id=id, 
+            python_callable=func,
+            system_site_packages=False,
+            requirements=REQUIREMENTS,
+            op_kwargs=op_kwargs
+        )
+        return task
+    '''
+    def gen_vpython(id, func, **opkwargs):
+        id = id
+        func = func
+        op_kw = opkwargs
+
+        task = PythonVirtualenvOperator(
+            task_id=id,
+            python_callable=func,
+            system_site_packages=False,
+            requirements=REQUIREMENTS,
+            op_kwargs=op_kw
+        )
+        return task
+    '''
+    # def pro_data(ds_nodash, url_param):
+        # print("pro data")
+
+    def pro_data(**params):
+        print("*" * 30)
+        print(params['task_name'])
+        print("*" * 30)
+
+    def pro_data2(task_name, **params):
+        print("@" * 30)
+        print(params['task_name'])
+        print("@" * 30)
 
     def merge_df():
         return
@@ -41,35 +89,40 @@ with DAG(
     def summary_df():
         return
     
-    start = EmptyOperator(task_id='start')
-    end = EmptyOperator(task_id='end')
-
-    apply_type = PythonVirtualenvOperator(
-            task_id="apply_type",
-            python_callable=app_type,
-            # requirements=["git+https://github.com/Seokxkyu/mov.git@0.3/api"],
-            system_site_packages=False
+    start, end = gen_empty('start', 'end')
+    
+    apply_type = gen_vpython(
+            id="apply_type",
+            func=pro_data,
+            op_kwargs={
+                "task_name": "apply_type!!!"
+            }
+    )
+    
+    
+    merge_df = gen_vpython(
+            id="merge_df",
+            func=pro_data,
+            op_kwargs={
+                "task_name": "merge_df!!!"
+            }
     )
 
-    merge_df = PythonVirtualenvOperator(
-            task_id="merge_df",
-            python_callable=merge_df,
-            # requirements=["git+https://github.com/Seokxkyu/mov.git@0.3/api"],
-            system_site_packages=False
+    de_dup = gen_vpython(
+            id="de_dup",
+            func=pro_data,
+            op_kwargs={
+                "task_name": "de_dup!!!"
+            }
     )
 
-    de_dup = PythonVirtualenvOperator(
-            task_id="del_dup",
-            python_callable=delete_duplicate,
-            # requirements=["git+https://github.com/Seokxkyu/mov.git@0.3/api"], 
-            system_site_packages=False
+    summary_df = gen_vpython(
+            id="summary_df",
+            func=pro_data,
+            op_kwargs={
+                "task_name": "summary_df!!!"
+            }
     )
 
-    summary_df = PythonVirtualenvOperator(
-            task_id="summary_df",
-            python_callable=summary_df,
-            # requirements=["git+https://github.com/Seokxkyu/mov.git@0.3/api"],
-            system_site_packages=False
-    )
 
-    start >> apply_type >> merge_df >> de_dup >> end
+    start >> apply_type >> merge_df >> de_dup >> summary_df >> end
